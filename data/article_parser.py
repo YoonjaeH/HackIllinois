706201b448ae613781e10349dcc2877f7ba62d3a
eunsunlee <eunsunl2@illinois.edu>
2021-04-09 22:51:17 -0500

    Update article_parser.py
    draft: google news api

--- data/article_parser.py
+++ data/article_parser.py
@@ -1,1 +1,18 @@
+## API KEY: fa2d8290ba3c4544b316dc90df84b47c
 import requests
+from newsapi import NewsApiClient
+
+API_KEY='fa2d8290ba3c4544b316dc90df84b47c'
+KEY_WORDS=[
+'discrimination',
+]
+
+'''
+ publishedAt -> timestamp
+'''
+if __name__ == '__main__':
+    api = NewsApiClient(api_key=API_KEY)
+    #news = api.get_everything(q='discrimination', language='en')
+    for key in KEY_WORDS:
+        news = api.get_everything(q=key, language='en')
+    print((news['totalResults']))
