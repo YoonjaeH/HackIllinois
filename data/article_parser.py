33641c09885b23e504c906e4f3d4e66b51c2908a
YoonjaeH <yjhwang411@gmail.com>
2021-04-09 23:04:57 -0500

    Update article_parser.py
    credential update

--- data/article_parser.py
+++ data/article_parser.py
@@ -1,18 +1,22 @@
-## API KEY: fa2d8290ba3c4544b316dc90df84b47c
 import requests
 from newsapi import NewsApiClient
+import credentials
 
-API_KEY='fa2d8290ba3c4544b316dc90df84b47c'
 KEY_WORDS=[
 'discrimination',
+'hate',
+'hatred',
+'racism',
+'asian hatred',
+'racial'
 ]
 
 '''
  publishedAt -> timestamp
 '''
 if __name__ == '__main__':
-    api = NewsApiClient(api_key=API_KEY)
+    api = NewsApiClient(api_key=credentials.API_KEY)
     #news = api.get_everything(q='discrimination', language='en')
     for key in KEY_WORDS:
         news = api.get_everything(q=key, language='en')
-    print((news['totalResults']))
+        print((news['totalResults']))
